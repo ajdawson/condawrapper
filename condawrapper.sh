84eda49c7889a82a02fb3e9b4ae4f0c4e4a59f4e
Andrew Dawson <ajdawson@acm.org>
2016-01-15 21:55:28 +0000

    Try and make the config directory.

--- condawrapper.sh
+++ condawrapper.sh
@@ -240,7 +240,7 @@ mkcondaenv () {
   fi
   # Create a condawrapper config for the environment:
   env_dir="$CONDAWRAPPER_HOME/$env_name"
-  if ! mkdir $env_dir; then
+  if ! mkdir -p $env_dir; then
     __err "error: failed to create condawrapper configuration directory: $env_dir"
     return 1
   fi
